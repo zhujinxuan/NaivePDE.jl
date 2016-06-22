ad5a95953f0b5c246d829b30b60b886df6d83568
zhujinxuan <zhujinxuan@gmail.com>
2016-06-22 14:21:24 -0400

    Seperating Iteractive Solvers

--- src/NaivePDE.jl
+++ src/NaivePDE.jl
@@ -1,23 +1,11 @@
 module NaivePDE
+using NaiveBoundary
 
 # package code goes here
 include("oneDopeartor.jl")
 include("TimeIntergral/TimeIntergral.jl")
-include("Boundaries/help_boundaries.jl")
-include("GridMatrixSolver/GridMatrixSolver.jl")
 include("hypobolicPDE/schemes.jl")
 
 export eval
 
-function list_eval{n}(f :: Function, data :: NTuple{n}...)
-  map((1:n...)) do ii
-    f( map(vv->(data[vv])[ii], (1:length(data)...))...)
-  end
-end
-
-function list_reduce{n}(f :: Function, data :: NTuple{n}...)
-  map((1:n...)) do ii
-    reduce(f, map(vv->(data[vv])[ii], (1:length(data)...)))
-  end
-end
 end # module
